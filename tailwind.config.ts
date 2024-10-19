23092010136bf2a8098720cacd7e535d906ddcba
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-19 10:05:41 +0700

    chore: add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,18 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          90: '#1b6d6d',
+          80: '#1f7f7f',
+          60: '#29a4a4',
+          50: '#2db6b6',
+        },
+        neutral: {
+          60: '#2e2e2e',
+          50: '#333333',
+          40: '#474747',
+          30: '#5c5c5c'
+        }
       },
     },
   },
